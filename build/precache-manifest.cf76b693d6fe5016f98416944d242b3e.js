f60fc5739aafe6d1ce7e785ed72dce3e624984f5
MatiMartinez <matia.martine2018@gmail.com>
2020-04-18 01:50:49 -0300

    Correccion homepage

---
+++ build/precache-manifest.cf76b693d6fe5016f98416944d242b3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c088372a30d48fcc01d4b990b96bfef",
+    "url": "/tp_5_react_lab4/index.html"
+  },
+  {
+    "revision": "85ee50137923f4c55f6b",
+    "url": "/tp_5_react_lab4/static/css/main.a9835bb0.chunk.css"
+  },
+  {
+    "revision": "100ee2990c37b90292e8",
+    "url": "/tp_5_react_lab4/static/js/2.5928d76a.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/tp_5_react_lab4/static/js/2.5928d76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ee50137923f4c55f6b",
+    "url": "/tp_5_react_lab4/static/js/main.f30de01f.chunk.js"
+  },
+  {
+    "revision": "9c5121b5386ddfcc3a2d",
+    "url": "/tp_5_react_lab4/static/js/runtime-main.cb9242e7.js"
+  },
+  {
+    "revision": "fd569b4914561eff1b32a2c143717771",
+    "url": "/tp_5_react_lab4/static/media/camion.fd569b49.png"
+  },
+  {
+    "revision": "ce01952771e5e36ab2c2554db5717913",
+    "url": "/tp_5_react_lab4/static/media/nro1.ce019527.jpg"
+  },
+  {
+    "revision": "c2da70b65aad4c7fe2d06627b478da7c",
+    "url": "/tp_5_react_lab4/static/media/nro3.c2da70b6.jpg"
+  },
+  {
+    "revision": "1850fbe9548eaa26a3a03d61cc4586d1",
+    "url": "/tp_5_react_lab4/static/media/ubicacion.1850fbe9.png"
+  }
+]);
